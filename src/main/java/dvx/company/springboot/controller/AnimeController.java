493ac7e8bdd49f9c688b7685a63a6cb26e8ea919
dalton <daltonchivambo@gmail.com>
2023-02-12 21:33:55 +0200

    Add: Method Get, Bad Request, stacktrace

--- src/main/java/dvx/company/springboot/controller/AnimeController.java
+++ src/main/java/dvx/company/springboot/controller/AnimeController.java
@@ -5,8 +5,10 @@ import dvx.company.springboot.service.AnimeService;
 import dvx.company.springboot.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,13 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/dvx/company/springboot/service/AnimeService.java
+++ src/main/java/dvx/company/springboot/service/AnimeService.java
@@ -1,14 +1,24 @@
 package dvx.company.springboot.service;
 
 import dvx.company.springboot.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
+    private List<Anime> animes =  List.of(new Anime(1L,"Vikings", "Disney"), new Anime(2L,"Mr Robot", "Netflix"));
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Vikings", "Disney"), new Anime(2L,"Mr Robot", "Netflix"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Anime not found"));
     }
 }
