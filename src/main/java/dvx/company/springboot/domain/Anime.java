1a91e4203c0bb786da9517fa22fc3ce38e7cd9ae
dalton <daltonchivambo@gmail.com>
2023-02-22 14:51:43 +0200

    Add: Spring Data JPA 1

--- src/main/java/dvx/company/springboot/domain/Anime.java
+++ src/main/java/dvx/company/springboot/domain/Anime.java
@@ -1,12 +1,21 @@
 package dvx.company.springboot.domain;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor //add cause jpa need constructor args
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String sponsor;
